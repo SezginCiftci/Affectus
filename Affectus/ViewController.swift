81df1856f9597f761147674a1f527ba45b5d9c8b
Sezgin Çiftçi <88513574+SezginCiftci@users.noreply.github.com>
2022-07-27 09:20:41 +0300

    Initial Commit

---
+++ Affectus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Affectus
+//
+//  Created by Sezgin Çiftci on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
